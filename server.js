f33d0da008940eca697385b21f3d92715804d924
TalhaYounas6 <myounas1168@gmail.com>
2025-06-06 12:59:54 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening at port 3000");
+});
+
+mongoose.connect(process.env.Database_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error(`There is an error ${error}`);
+});
+db.once("open", () => {
+  console.log("Database Connected");
+});
